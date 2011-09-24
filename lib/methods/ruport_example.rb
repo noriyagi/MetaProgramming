69c8ac01afb17d8ff28c3026c7d56bbdcdbbf133
Noriaki Koyanagi <koyanagn@gmail.com>
2011-09-24 10:18:41 +0900

    Prat2

--- lib/methods/ruport_example.rb
+++ lib/methods/ruport_example.rb
@@ -1,4 +1,7 @@
+require 'rubygems'
 require 'ruport'
+
+puts Gem.latest_load_paths()
 
 table = Ruport::Data::Table.new :column_names => ["country", "wine"],
                 :data => [["France", "Bordeaux"],["Itary","Chianti"],
