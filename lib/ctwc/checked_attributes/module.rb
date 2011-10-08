6eec2fc49933bc8658b6d3ff503ffea93e7cb544
Noriaki Koyanagi <koyanagn@gmail.com>
2011-10-08 16:33:22 +0900

    Part1 fin

---
+++ lib/ctwc/checked_attributes/module.rb
@@ -0,0 +1,46 @@
+require 'test/unit'
+
+module CheckedAttributes
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |value|
+      raise 'Invalid attribute' unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+
+    define_method attribute do
+      instance_variable_get "@#{attribute}"
+    end
+  end
+  end
+end
+
+class Person
+  include CheckedAttributes
+
+  attr_checked :age do |v|
+    v >= 18
+  end
+end
+
+class TestCheckedAttribute < Test::Unit::TestCase
+  def setup
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_invalid_values
+    assert_raise RuntimeError, 'Invalid attritude' do
+      @bob.age = 17
+    end
+  end
+end
+
